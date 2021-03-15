e08fa8a8dc13c6fe43160b4a89c3aa0689a769d8
Martin Guo <gbforever@163.com>
2021-03-15 09:44:23 +0800

    Update gen feature method

--- py/create_training_csv.py
+++ py/create_training_csv.py
@@ -4,10 +4,7 @@ from tensorflow.keras.applications.vgg16 import preprocess_input
 from tensorflow.keras.models import Model
 import numpy as np
 import pandas as pd
-
-
-
-
+from tqdm import tqdm
 
 def conver_image_to_features(model, img_path, img_resize = (224, 224)):
     img = image.load_img(img_path, target_size=img_resize)
@@ -15,36 +12,53 @@ def conver_image_to_features(model, img_path, img_resize = (224, 224)):
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
     features = model.predict(x)
-    return features.reshape(-1) # convert to 1 dimention
+    return features.reshape(-1)
 
-
-def create_train_data_from_images(model, train_csv):
+'''
+def create_train_data_from_images(model, train_csv, decimals=4):
     df = pd.read_csv(train_csv)
 
     image_features = []
 
-    for ind, row in df.iterrows():
-        print("processing {} of {}".format(ind + 1, df.shape[0]))
+    for ind, row in tqdm(df.iterrows(), total=df.shape[0]):
         image_features.append(
-            (conver_image_to_features(model, row["img1"]),
-             conver_image_to_features(model, row["img2"]),
+            (conver_image_to_features(model, row["img1"], decimals=decimals).tolist(),
+             conver_image_to_features(model, row["img2"], decimals=decimals).tolist(),
              row["same"]))
 
     res = pd.DataFrame({
         "x1": [x[0] for x in image_features],
         "x2": [x[1] for x in image_features],
-        "y": [x[2] for x in image_features],
+        "y":  [x[2] for x in image_features],
     })
 
     return res
+'''
 
+def create_features_dataframe_by_diff_feature(model, train_csv):
+    df = pd.read_csv(train_csv)
 
-def genearte_image_feature_file(csv_file, output):
+    image_features = []
+
+    for ind, row in tqdm(df.iterrows(), total=df.shape[0]):
+
+        try:
+            feature = conver_image_to_features(model, row["img1"]) - \
+                      conver_image_to_features(model, row["img2"])
+
+            image_features.append(np.append(feature, row["same"]))
+        except:
+            print("Error img {} {}".format(row["img1"], row["img2"]))
+
+    return pd.DataFrame().from_records(image_features)
+
+def genearte_image_feature_file(input, output, decimal_number=3):
+
     # last layer
     model = VGG16(weights="imagenet", include_top=False)
-
-    res_df = create_train_data_from_images(model, csv_file)
-    res_df.to_csv(output, index=False)
+    res_df = create_features_dataframe_by_diff_feature(model, input)
+    fplace = '%.{}f'.format(decimal_number)
+    res_df.to_csv(output, index=False, header=False, float_format=fplace)
 
 if __name__ == '__main__':
-    genearte_image_feature_file("./min_index.csv", "min_feature.csv")
+    genearte_image_feature_file("./train_10000.csv", "min_feature.csv")
